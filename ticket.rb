c69cf335c03a53bb4faa06c4d809874e177e78a5
armzig <bellotaophyc@outlook.com>
2023-07-08 21:11:06 +0100

    using string interpolation to insert values of expressions into the string

--- ticket.rb
+++ ticket.rb
@@ -1,1 +1,33 @@
 #!/usr/bin/ruby
+
+ticket = Object.new
+
+def ticket.venue
+    "Town Hall"
+end
+
+def ticket.performer
+    "Mark Twain"
+end
+
+def ticket.event
+    "Author's reading"
+end
+
+def ticket.price
+    5.50
+end
+
+def ticket.seat
+    "Second Balcony, row J, seat 12"
+end
+
+def ticket.date
+    "08/07/2023"
+end
+
+
+puts "This ticket is for: #{ticket.event}, at #{ticket.venue}. " + 
+    "The performer is #{ticket.performer}. " +
+    "The seat is #{ticket.seat}, " +
+    "and it costs $#{"%.2f." % ticket.price}"
